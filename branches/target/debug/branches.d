fe9b67fae69e15ffda4a9dd7f078257024202f2e
Josh Alvarado <Thunderblarg@gmail.com>
2024-03-21 12:10:16 -0700

    Finished chapter 3, Common Programming Concepts

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\jalva12x\Documents\repos\trpl\branches\target\debug\branches.exe: C:\Users\jalva12x\Documents\repos\trpl\branches\src\main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+C:\Users\jalva12x\Documents\repos\trpl\branches\target\debug\deps\branches.exe: src\main.rs
+
+C:\Users\jalva12x\Documents\repos\trpl\branches\target\debug\deps\branches.d: src\main.rs
+
+src\main.rs:
