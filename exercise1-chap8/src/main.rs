ba0ccb9c7e0557dba60b97c733a12697bc3cd433
Josh Alvarado <Thunderblarg@gmail.com>
2024-04-03 15:58:15 -0700

    Going through TRPL Chapter 8 end of chapter exercises

---
+++ exercise1-chap8/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // Given a list of integers, use a vector
+    let mut v = vec[4, 6, 2, 9, 1, 7, 3, 5, 8, 2,
+                0, 7, 5, 9, 3, 8, 1, 4, 6, 10,
+                2, 5, 8, 3, 9, 7, 1, 4, 0, 6,
+                10, 9, 2, 5, 8, 1, 3, 7, 4, 0,
+                6, 10, 2, 5, 8, 3, 9, 7, 1, 4];
+
+    // And return the median when sorted (the value in the middle position)
+    v.sort();
+
+    // okay, but what does that MEAN though, like do they want whatever value falls
+    // in the middle of the vector when it's sorted?
+
+    // Or do they want the median value of all available values?
+
+
+
+
+}
