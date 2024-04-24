d3591bb6b5536ce48815e6d610771f4aab477a3b
Thunderblarg <Thunderblarg@gmail.com>
2024-04-24 16:55:40 -0700

    Wasn't much to do here, but done with TRPL chapter 10

---
+++ panic/target/debug/deps/panic.d
@@ -0,0 +1,5 @@
+C:\Users\Josh\Documents\repos\trpl\panic\target\debug\deps\panic.exe: src\main.rs
+
+C:\Users\Josh\Documents\repos\trpl\panic\target\debug\deps\panic.d: src\main.rs
+
+src\main.rs:
---
+++ panic/target/debug/panic.d
@@ -0,0 +1,1 @@
+C:\Users\Josh\Documents\repos\trpl\panic\target\debug\panic.exe: C:\Users\Josh\Documents\repos\trpl\panic\src\main.rs
